ba3323e176461b5c66118660a2b925470382a21d
Kevin Karma <kevinkarma55@gmail.com>
2019-09-02 22:59:33 +0000

    Done in an hour, yeahhh

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -6,5 +6,39 @@ class Artist
     @name = name
     @years_experience = years_experience
   end
+  
+  def paintings 
+    Painting.all.select { |painting| painting.artist == self } 
+  end
+  
+  def galleries
+    self.paintings.map { |painting| painting.gallery }
+  end
+  
+  def cities 
+    self.galleries.map { |gallery| gallery.city }
+  end
+  
+  def create_painting(title,price,gallery)
+    Painting.new(title,price,gallery,self)
+  end
+  
+  def self.total_experience
+    allArtist = Painting.all.map { |painting| painting.artist }
+    allArtist = allArtist.uniq
+    allExp = allArtist.map { |artist| artist.years_experience }
+    allExp.sum 
+  end
+  
+  def self.most_prolific
+    allArtist = Painting.all.map { |painting| painting.artist }
+    allArtist.max_by { |artist| artist.years_experience }
+  end
+  
+  def self.all 
+    Painting.all.map { |painting| painting.artist } 
+  end
+  
+  
 
 end
--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -6,5 +6,26 @@ class Gallery
     @name = name
     @city = city
   end
+  
+  def paintings
+    allPaintings = Painting.all.select { |painting| painting.gallery == self } 
+  end
+  
+  def artists
+    self.paintings.map { |painting| painting.artist }
+  end
+  
+  def artist_name
+    self.artists.map { |artist| artist.name }
+  end
+  
+  def most_expensive_painting
+    self.paintings.max_by { |painting| painting.price } 
+  end
+  
+  def self.all 
+    allGallery = Painting.all.map { |painting| painting.gallery }
+    allGallery.uniq 
+  end
 
 end
